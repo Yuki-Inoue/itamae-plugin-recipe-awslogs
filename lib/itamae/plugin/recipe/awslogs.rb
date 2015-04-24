7b48edfb8cafa982280c5f2f03a8401554354102
Daisuke Hirakiuchi <daisuke.hirakiuchi@leonisand.co>
2015-04-24 15:17:06 +0900

    initial import

---
+++ lib/itamae/plugin/recipe/awslogs.rb
@@ -0,0 +1,11 @@
+require "itamae/plugin/recipe/awslogs/version"
+
+module Itamae
+  module Plugin
+    module Recipe
+      module Awslogs
+        # Your code goes here...
+      end
+    end
+  end
+end
